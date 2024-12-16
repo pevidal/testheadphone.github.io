0c57a128c5f8ebc54ec0109d656702e118cb28ad
Pedro Henrique Santana Vidal <pedrovidal_pedro@hotmail.com>
2024-12-16 12:00:26 -0300

    player

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function playAudioLeft() {
+    const audio = document.getElementById("audioPlayerLeft");
+    audio.play();
+}
+function playAudioRight() {
+    const audio = document.getElementById("audioPlayerRight");
+    audio.play();
+}
